ae3355ea02b671ef6e11759d9d17d4b3a55a6874
Dan Carlson <dancarlson@gmail.com>
2022-12-15 17:49:08 -0500

    make view controller host rudimentary tab bar

--- App/Marvelo/ViewController.swift
+++ App/Marvelo/ViewController.swift
@@ -6,36 +6,52 @@
 //
 
 import UIKit
-import APIClient
-import Models
+
+import Characters
 
 class ViewController: UIViewController {
-
-	var characters: [Character] = []
-	var events: [Event] = []
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
-		Task {
-			await fetchResults()
-			DispatchQueue.main.async {
-				self.view.backgroundColor = .green
-			}
-		}
-		self.view.backgroundColor = .red
 	}
 	
-	func fetchResults() async {
-		do {
-			let characters = await try APIClient.fetchCharacters()
-			let events = await try APIClient.fetchEvents()
-			print("\(characters.count) characters, \(events.count) events")
-		} catch {
-			print("failed")
-		}
+	override func viewDidAppear(_ animated: Bool) {
+		let tabBarController = UITabBarController()
+		let charactersVC = Characters.CharactersViewController()
+		tabBarController.setViewControllers([charactersVC], animated: false)
+		self.present(tabBarController, animated: false)
 	}
-
-
 }
 
+
+
+//class ViewController: UIViewController {
+//
+//	var characters: [Character] = []
+//	var events: [Event] = []
+//
+//	override func viewDidLoad() {
+//		super.viewDidLoad()
+//		// Do any additional setup after loading the view.
+//		Task {
+//			await fetchResults()
+//			DispatchQueue.main.async {
+//				self.view.backgroundColor = .green
+//			}
+//		}
+//		self.view.backgroundColor = .red
+//	}
+//
+//	func fetchResults() async {
+//		do {
+//			let characters = try await APIClient.fetchCharacters()
+//			let events = try await APIClient.fetchEvents()
+//			print("\(characters.count) characters, \(events.count) events")
+//		} catch {
+//			print("failed")
+//		}
+//	}
+//
+//
+//}
+//
